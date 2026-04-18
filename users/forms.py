1cd28d1ee40c598a0487379a044f9b3da13cd0f3
Enrique Gálvez <kikega@telefonica.net>
2026-04-19 00:38:44 +0200

    Inputs del login con placeholder flotante

--- users/forms.py
+++ users/forms.py
@@ -17,10 +17,10 @@ class CustomUserChangeForm(UserChangeForm):
 class CustomAuthenticationForm(AuthenticationForm):
     username = forms.EmailField(
         label=_("Correo electrónico"),
-        widget=forms.EmailInput(attrs={'autofocus': True, 'class': 'mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-primary focus:ring-primary sm:text-sm', 'placeholder': 'tu@email.com'})
+        widget=forms.EmailInput(attrs={'autofocus': True, 'class': 'peer block w-full appearance-none rounded-md border border-gray-300 bg-transparent px-3 pb-2 pt-5 text-sm text-gray-900 focus:border-primary focus:outline-none focus:ring-1 focus:ring-primary', 'placeholder': ' '})
     )
     password = forms.CharField(
         label=_("Contraseña"),
         strip=False,
-        widget=forms.PasswordInput(attrs={'autocomplete': 'current-password', 'class': 'mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-primary focus:ring-primary sm:text-sm', 'placeholder': '••••••••'})
+        widget=forms.PasswordInput(attrs={'autocomplete': 'current-password', 'class': 'peer block w-full appearance-none rounded-md border border-gray-300 bg-transparent px-3 pb-2 pt-5 text-sm text-gray-900 focus:border-primary focus:outline-none focus:ring-1 focus:ring-primary', 'placeholder': ' '})
     )
